168408566f533c9bc0fbb82b9e82f239b63267e9
Alexandre Lima <alexandreslima@outlook.com>
2020-03-05 16:22:58 -0300

    add news api signature plan call

---
+++ 1 - Libraries/InfinitusApp.Services/Signature/SignaturePlanApplicationUserService.cs
@@ -0,0 +1,27 @@
+﻿using InfinitusApp.Core.Data.Commands;
+using InfinitusApp.Core.Data.DataModels.Signature;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfinitusApp.Services.Signature
+{
+    public class SignaturePlanApplicationUserService : ServiceBase
+    {
+        public SignaturePlanApplicationUserService(InfinitusAppServiceClient _serviceClient) : base(_serviceClient)
+        {
+        }
+
+        public async Task<SignaturePlanApplicationUser> Create(CreateSignaturePlanApplicationUserCommand createCommand)
+        {
+            return await ServiceClient.InvokeApiAsync<CreateSignaturePlanApplicationUserCommand, SignaturePlanApplicationUser>("SignaturePlanApplicationUser/Create", createCommand);
+        }
+
+        public async Task<List<SignaturePlanApplicationUser>> GetAll()
+        {
+            return await ServiceClient.MobileServiceClient.InvokeApiAsync<List<SignaturePlanApplicationUser>>("SignaturePlanApplicationUser/GetAll", HttpMethod.Get, null);
+        }
+    }
+}
---
+++ 1 - Libraries/InfinitusApp.Services/Signature/SignaturePlanPaymentHistoryService.cs
@@ -0,0 +1,21 @@
+﻿using InfinitusApp.Core.Data.DataModels.Signature;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfinitusApp.Services.Signature
+{
+    public class SignaturePlanPaymentHistoryService : ServiceBase
+    {
+        public SignaturePlanPaymentHistoryService(InfinitusAppServiceClient _serviceClient) : base(_serviceClient)
+        {
+        }
+
+        public async Task<List<SignaturePlanPaymentHistory>> GetAll()
+        {
+            return await ServiceClient.MobileServiceClient.InvokeApiAsync<List<SignaturePlanPaymentHistory>>("SignaturePlanPaymentHistory/GetAll", HttpMethod.Get, null);
+        }
+    }
+}
