03c174457a20417bf4f7c4fb094448b328786ee4
Rodrigo Mafei <rodrigomafei@live.com>
2020-03-11 11:53:30 -0300

    add paymentcondition service

---
+++ 1 - Libraries/InfinitusApp.Services/FInancial/PaymentConditionService.cs
@@ -0,0 +1,47 @@
+﻿using InfinitusApp.Core.Data.DataModels;
+using InfinitusApp.Core.Extensions;
+using OData.QueryBuilder.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfinitusApp.Services.FInancial
+{
+    public class PaymentConditionService : ServiceBase
+    {
+        public PaymentConditionService(InfinitusAppServiceClient _serviceClient) : base(_serviceClient)
+        {
+        }
+
+        public async Task<List<PaymentCondition>> GetAll(Expression<Func<PaymentCondition, bool>> entityFilter = null, Expression<Func<PaymentCondition, object>> entityOrderBy = null, int? skip = null, int? top = null, bool desc = false)
+        {
+            var odataBuilder = new ODataQueryBuilder<PaymentCondition>("")
+                    .For<PaymentCondition>(x => x)
+                    .ByList();
+
+            if (top.HasValue)
+                odataBuilder.Top(top.Value);
+
+            if (skip.HasValue)
+                odataBuilder.Skip(skip.Value);
+
+            if (entityFilter != null)
+                odataBuilder.Filter(entityFilter);
+
+            if (entityOrderBy != null)
+            {
+                if (desc)
+                    odataBuilder.OrderByDescending(entityOrderBy);
+
+                else
+                    odataBuilder.OrderBy(entityOrderBy);
+            }
+
+            var dic = odataBuilder.ToDictionary();
+
+            return await ServiceClient.MobileServiceClient.InvokeApiAsync<List<PaymentCondition>>("PaymentCondition/GetAll", HttpMethod.Get, dic);
+        }
+    }
+}
---
+++ Z - Tests/UnitTestProject/PaymentConditionTest.cs
@@ -0,0 +1,29 @@
+﻿using InfinitusApp.Services.FInancial;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class PaymentConditionTest : _0___TestBase
+    {
+        public PaymentConditionService PaymentConditionService { get { return new PaymentConditionService(Client); } }
+
+        [TestMethod]
+        public async Task GetAll()
+        {
+            try
+            {
+                var list = await PaymentConditionService.GetAll();
+            }
+
+            catch(Exception e)
+            {
+
+            }
+        }
+    }
+}
