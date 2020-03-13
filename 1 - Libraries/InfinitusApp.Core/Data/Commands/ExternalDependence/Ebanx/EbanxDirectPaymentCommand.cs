1200bffa804b400275dd4d48055a948d0b901f7f
Rodrigo Mafei <rodrigomafei@live.com>
2020-03-13 11:00:22 -0300

    ebanx service

---
+++ 1 - Libraries/InfinitusApp.Core/Data/Commands/ExternalDependence/Ebanx/EbanxDirectPaymentCommand.cs
@@ -0,0 +1,13 @@
+﻿using Ebanx.net.Commands.PaymentApi;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfinitusApp.Core.Data.Commands.ExternalDependence.Ebanx
+{
+    public class EbanxDirectPaymentCommand
+    {
+        public string FinancialRequestId { get; set; }
+        public DirectCommand DirectCommand { get; set; }
+    }
+}
---
+++ 1 - Libraries/InfinitusApp.Services/ExternalDependence/Ebanx/EbanxService.cs
@@ -0,0 +1,33 @@
+﻿using Ebanx.net.Parameters.Responses.DirectOperation;
+using Ebanx.net.Parameters.Responses.QueryOperation;
+using InfinitusApp.Core.Data.Commands.ExternalDependence.Ebanx;
+using InfinitusApp.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfinitusApp.Services.ExternalDependence.Ebanx
+{
+    public class EbanxService : ServiceBase
+    {
+        public EbanxService(InfinitusAppServiceClient _serviceClient) : base(_serviceClient)
+        {
+        }
+
+        public async Task<DirectResponse> DirectPayment(EbanxDirectPaymentCommand cmd)
+        {
+            return await ServiceClient.MobileServiceClient.InvokeApiAsync<EbanxDirectPaymentCommand, DirectResponse>("Ebanx/DirectPayment", cmd);
+        }
+
+        public async Task<List<QueryResponse>> GetInvoice(string hash)
+        {
+            var dic = new Dictionary<string, string>
+            {
+                {"hash", hash }
+            };
+
+            return await ServiceClient.MobileServiceClient.InvokeApiAsync<List<QueryResponse>>("Ebanx/GetInvoice", HttpMethod.Get, dic);
+        }
+    }
+}
