859f457fa6f914e9aac8b26b696dd59bd8b476b6
Rodrigo Mafei <rodrigomafei@live.com>
2020-04-02 19:04:12 -0300

    .

--- 1 - Libraries/InfinitusApp.Core/Data/DataModels/OpeningHours.cs
+++ 1 - Libraries/InfinitusApp.Core/Data/DataModels/OpeningHours.cs
@@ -114,6 +114,21 @@ namespace InfinitusApp.Core.Data.DataModels
             }
         }
 
+        public static string GetDayPresentation(int day)
+        {
+            switch (day)
+            {
+                case 1: return "Segunda-Feira";
+                case 2: return "Terça-Feira";
+                case 3: return "Quarta-Feira";
+                case 4: return "Quinta-Feira";
+                case 5: return "Sexta-Feira";
+                case 6: return "Sábado";
+                case 7: return "Domingo";
+                default: return "Valor inválido";
+            }
+        }
+
         #endregion
     }
 }
