2e9659981568a8f00defe61b023b8ddd6540f17b
Alexandre Lima <alexandreslima@outlook.com>
2020-04-06 17:53:33 -0300

    Renomeado estabelecimento para negócio

--- 1 - Libraries/InfinitusApp.Core/Data/Enums/DataItemEnums.cs
+++ 1 - Libraries/InfinitusApp.Core/Data/Enums/DataItemEnums.cs
@@ -60,7 +60,7 @@ namespace InfinitusApp.Core.Data.Enums
                 case DataItemType.Book:
                     return plural ? "Livros" : "Livro";
                 case DataItemType.Company:
-                    return plural ? "Estabelecimentos" : "Estabelecimento";
+                    return plural ? "Negócio" : "Negócios";
                 case DataItemType.Pet:
                     return plural ? "Animais de Estimação" : "Animal de Estimação";
                 case DataItemType.PropertyTenancy:
@@ -109,7 +109,7 @@ namespace InfinitusApp.Core.Data.Enums
                 case "Book":
                     return plural ? "Livros" : "Livro";
                 case "Company":
-                    return plural ? "Estabelecimentos" : "Estabelecimento";
+                    return plural ? "Negócios" : "Negócio";
                 case "Pet":
                     return plural ? "Animais de Estimação" : "Animal de Estimação";
                 case "PropertyTenancy":
