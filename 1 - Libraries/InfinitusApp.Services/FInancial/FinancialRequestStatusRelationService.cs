28ebd0305f69e856e6d846c3ba717dae6a59e590
Rodrigo Mafei <rodrigomafei@live.com>
2020-04-24 15:51:10 -0300

    status financial request services

---
+++ 1 - Libraries/InfinitusApp.Services/FInancial/FinancialRequestStatusRelationService.cs
@@ -0,0 +1,22 @@
+﻿using InfinitusApp.Core.Data.Commands;
+using InfinitusApp.Core.Data.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfinitusApp.Services.FInancial
+{
+    public class FinancialRequestStatusRelationService : ServiceBase
+    {
+        public FinancialRequestStatusRelationService(InfinitusAppServiceClient _serviceClient) : base(_serviceClient)
+        {
+
+        }
+
+        public async Task<FinancialRequestStatusRelation> Create(CreateFinancialRequestStatusRelationCommand createCommand)
+        {
+            return await ServiceClient.InvokeApiAsync<CreateFinancialRequestStatusRelationCommand, FinancialRequestStatusRelation>("FinancialRequestStatusRelation/Create", createCommand);
+        }
+    }
+}
---
+++ 1 - Libraries/InfinitusApp.Services/FInancial/StatusFinancialRequestService.cs
@@ -0,0 +1,49 @@
+﻿using InfinitusApp.Core.Data.DataModels;
+using InfinitusApp.Core.Extensions;
+using OData.QueryBuilder.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfinitusApp.Services.FInancial
+{
+    public class StatusFinancialRequestService : ServiceBase
+    {
+        public StatusFinancialRequestService(InfinitusAppServiceClient _serviceClient) : base(_serviceClient)
+        {
+
+        }
+
+        public async Task<List<StatusFinancialRequest>> GetAll(Expression<Func<StatusFinancialRequest, bool>> entityFilter = null, Expression<Func<StatusFinancialRequest, object>> entityOrderBy = null, int? skip = null, int? top = null, bool desc = false)
+        {
+            var odataBuilder = new ODataQueryBuilder<StatusFinancialRequest>("")
+                    .For<StatusFinancialRequest>(x => x)
+                    .ByList();
+
+            if (top.HasValue)
+                odataBuilder.Top(top.Value);
+
+            if (skip.HasValue)
+                odataBuilder.Skip(skip.Value);
+
+            if (entityFilter != null)
+                odataBuilder.Filter(entityFilter);
+
+            if (entityOrderBy != null)
+            {
+                if (desc)
+                    odataBuilder.OrderByDescending(entityOrderBy);
+
+                else
+                    odataBuilder.OrderBy(entityOrderBy);
+            }
+
+            var dic = odataBuilder.ToDictionary();
+
+            return await ServiceClient.MobileServiceClient.InvokeApiAsync<List<StatusFinancialRequest>>("StatusFinancialRequest/GetAll", HttpMethod.Get, dic);
+        }
+
+    }
+}
