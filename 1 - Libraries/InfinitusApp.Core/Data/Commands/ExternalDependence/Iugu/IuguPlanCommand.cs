e5861c842b9af0e4128c6e7ce319d99705c825ba
Rodrigo Mafei <rodrigomafei@live.com>
2020-08-05 17:53:52 -0300

    iugu

---
+++ 1 - Libraries/InfinitusApp.Core/Data/Commands/ExternalDependence/Iugu/IuguPlanCommand.cs
@@ -0,0 +1,31 @@
+﻿using InfinitusApp.Core.Data.DataModels.External.Iugu;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfinitusApp.Core.Data.Commands.ExternalDependence.Iugu
+{
+    public class IuguPlanCommand
+    {
+        public string MarketplaceId { get; set; }
+        public PlanRequestMessage IuguCommand { get; set; }
+    }
+
+
+    public class CreateIuguPlanCommand : IuguPlanCommand
+    {
+        public string SignaturePlanId { get; set; }
+    }
+
+    public class UpdateIuguPlanCommand : IuguPlanCommand
+    {
+
+        public string PlanId { get; set; }
+    }
+
+    public class DeleteIuguPlanCommand
+    {
+        public string PlanId { get; set; }
+        public string MarketplaceId { get; set; }
+    }
+}
