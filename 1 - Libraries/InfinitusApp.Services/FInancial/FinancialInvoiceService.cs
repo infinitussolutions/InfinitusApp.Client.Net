4a6dc8e39ba34f341b81bf2ad838e7e1d40837fe
Rodrigo Mafei <rodrigomafei@live.com>
2020-09-17 18:13:23 -0300

    financial invoice

---
+++ 1 - Libraries/InfinitusApp.Services/FInancial/FinancialInvoiceService.cs
@@ -0,0 +1,39 @@
+﻿using InfinitusApp.Core.Data.DataModels;
+using InfinitusApp.Core.Extensions;
+using OData.QueryBuilder.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfinitusApp.Services.FInancial
+{
+    public class FinancialInvoiceService : ServiceBase
+    {
+        public FinancialInvoiceService(InfinitusAppServiceClient _serviceClient) : base(_serviceClient)
+        {
+
+        }
+
+        public async Task<List<FinancialRequest>> GetAllByDataStoreId(string dataStoreId, Expression<Func<FinancialInvoice, bool>> entityFilter = null, int skip = 0, int top = 10)
+        {
+            var odataBuilder = new ODataQueryBuilder<FinancialRequest>("")
+                 .For<FinancialInvoice>(x => x)
+                 .ByList()
+                 .Top(top)
+                 .Skip(skip)
+                 .OrderByDescending(x => x.CreatedAt)
+                 ;
+
+            if (entityFilter != null)
+                odataBuilder.Filter(entityFilter);
+
+            var dic = odataBuilder.ToDictionary();
+
+            dic.Add("dataStoreId", dataStoreId);
+
+            return await ServiceClient.InvokeApiAsync<List<FinancialRequest>>("FinancialInvoice/GetAllByDataStoreId", HttpMethod.Get, dic);
+        }
+    }
+}
