c032f2b5a21c59d83cc85fcbfcbea712832a9c10
Alexandre Lima <alexandreslima@outlook.com>
2020-10-01 15:55:07 -0300

    new prop

--- 1 - Libraries/InfinitusApp.Core/Data/DataModels/FinancialRequestTakeAwayInfo.cs
+++ 1 - Libraries/InfinitusApp.Core/Data/DataModels/FinancialRequestTakeAwayInfo.cs
@@ -6,6 +6,8 @@ namespace InfinitusApp.Core.Data.DataModels
 {
     public class FinancialRequestTakeAwayInfo
     {
-        public DateTime DateToTake { get; set; }
+        public DateTime? DateToTake { get; set; }
+
+        public string DateToTakePresentation => DateToTake.HasValue ? DateToTake.Value.ToString("dd/MM/yy") : "";
     }
 }
