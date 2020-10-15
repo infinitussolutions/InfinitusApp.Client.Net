7248214605206ce309de3e9e32e1945da6016051
Alexandre Lima <alexandreslima@outlook.com>
2020-10-15 11:59:05 -0300

    add combo service

---
+++ 1 - Libraries/InfinitusApp.Services/ComboServices/ComboItemService.cs
@@ -0,0 +1,37 @@
+﻿using InfinitusApp.Core.Data.Commands;
+using InfinitusApp.Core.Data.DataModels.ComboModels;
+using InfinitusApp.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfinitusApp.Services.ComboServices
+{
+    public class ComboItemService : ServiceBase
+    {
+        public ComboItemService(InfinitusAppServiceClient _serviceClient) : base(_serviceClient)
+        {
+        }
+
+        public async Task<ComboItem> Create(CreateComboItemCommand cmd)
+        {
+            return await ServiceClient.InvokeApiAsync<CreateComboItemCommand, ComboItem>(nameof(ComboItem) + "/Create", cmd);
+        }
+
+        public async Task<ComboItem> Update(UpdateComboItemCommand cmd)
+        {
+            return await ServiceClient.InvokeApiAsync<UpdateComboItemCommand, ComboItem>(nameof(ComboItem) + "/Update", cmd, HttpMethod.Patch, null);
+        }
+
+        public async Task<ComboItem> GetById(string id)
+        {
+            var dic = new Dictionary<string, string>
+            {
+                {"id",id }
+            };
+
+            return await ServiceClient.MobileServiceClient.InvokeApiAsync<ComboItem>(nameof(ComboItem) + "/GetById", HttpMethod.Get, dic);
+        }
+    }
+}
---
+++ 1 - Libraries/InfinitusApp.Services/ComboServices/ComboService.cs
@@ -0,0 +1,71 @@
+﻿using InfinitusApp.Core.Data.Commands;
+using InfinitusApp.Core.Data.DataModels.ComboModels;
+using InfinitusApp.Core.Extensions;
+using OData.QueryBuilder.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfinitusApp.Services.ComboServices
+{
+    public class ComboService : ServiceBase
+    {
+        public ComboService(InfinitusAppServiceClient _serviceClient) : base(_serviceClient)
+        {
+        }
+
+        public async Task<Combo> Create(CreateComboCommand cmd)
+        {
+            return await ServiceClient.InvokeApiAsync<CreateComboCommand, Combo>(nameof(Combo) + "/Create", cmd);
+        }
+
+        public async Task<Combo> Update(UpdateComboCommand cmd)
+        {
+            return await ServiceClient.InvokeApiAsync<UpdateComboCommand, Combo>(nameof(Combo) + "/Update", cmd, HttpMethod.Patch, null);
+        }
+
+        public async Task<Combo> GetById(string id)
+        {
+            var dic = new Dictionary<string, string>
+            {
+                {"id",id }
+            };
+
+            return await ServiceClient.MobileServiceClient.InvokeApiAsync<Combo>(nameof(Combo) + "/GetById", HttpMethod.Get, dic);
+        }
+
+        public async Task<List<Combo>> GetAll(Expression<Func<Combo, bool>> entityFilter = null, Expression<Func<Combo, object>> entityOrderBy = null, int? skip = null, int? top = null, bool desc = false)
+        {
+            var odataBuilder = new ODataQueryBuilder<Combo>("")
+                    .For<Combo>(x => x)
+                    .ByList();
+
+            if (skip.HasValue)
+                odataBuilder.Skip(skip.Value);
+
+            if (top.HasValue)
+                odataBuilder.Top(top.Value);
+
+            if (entityFilter != null)
+                odataBuilder.Filter(entityFilter);
+
+            if (entityOrderBy != null)
+            {
+                if (desc)
+                    odataBuilder.OrderBy(entityOrderBy);
+
+                else
+                    odataBuilder.OrderBy(entityOrderBy);
+            }
+
+            else
+                odataBuilder.OrderByDescending(x => x.CreatedAt);
+
+            var dic = odataBuilder.ToDictionary();
+
+            return await ServiceClient.InvokeApiAsync<List<Combo>>(nameof(Combo) + "/GetAll", HttpMethod.Get, dic);
+        }
+    }
+}
